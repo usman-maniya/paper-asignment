281cf2c4561f8c2cdba18396816ec5f760048aec
usman-maniya <55947865+usman-maniya@users.noreply.github.com>
2019-10-24 22:51:52 +0500

    Add files via upload

---
+++ paper 10 24 19.vb
@@ -0,0 +1,36 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim s1 As Strings
+        Dim validbinarystring As Boolean
+        Dim stringsize As Integer
+        Dim stringchar As Char
+        Dim denary As Integer
+
+        validbinarystring = True
+
+        Print(" Enter binary number ")
+        Input(s1)
+
+       If 
+            If stringsize < 7 Or stringsize > 0 Then
+                validbinarystring = False
+            End If
+
+        If stringchar ≠ 0 or stringchar ≠ 1 Then
+                validbinarystring = False
+            End If
+
+            then
+
+            Print(" binary number is invalid ")
+        Else
+
+            s1 = denary
+
+        End If
+
+        Print(denary value is "denary")
+    End Sub
+
+End Module
